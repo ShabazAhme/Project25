039ca7794036150ee265697e49c395db8feb5986
ShabazAhme <64406126+ShabazAhme@users.noreply.github.com>
2020-05-16 13:21:00 +0530

    Update Tanker.js

--- Tanker.js
+++ Tanker.js
@@ -4,17 +4,20 @@ class Tank {
       isStatic: true
      }
     
-    this.body = Bodies.rectangle(x, y, width,height, options);
+    this.body1 = Bodies.rectangle(x,y,width,height,options);
+    this.body2 = Bodies.rectangle(x,y,width,height,options);
     this.width = width;
     this.height = height;
       
-    World.add(world, this.body);
+    World.add(world,this.body1);
+    World.add(world,this.body2);
     }
     display(){
       var pos =this.body.position;
       translate(pos.x, pos.y);
       fill(255);
-      rect(0,0,this.width, this.height);
+      rect(this.body1.position.x,this.body1.position.y,this.width,this.height);
+      rect(this.body2.position.x,this.body2.position.y,this.width,this.height);
       rectMode(CENTER);
     };
 }
