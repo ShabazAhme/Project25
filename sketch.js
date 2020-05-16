9b9b587f7091431e02e4634bc8f56946ff70ccd2
ShabazAhme <64406126+ShabazAhme@users.noreply.github.com>
2020-05-16 13:27:53 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ function setup() {
    engine = Engine.create();
    world = engine.world;   
    
-   ground = new Ground(450,595,900,10);
+   ground = new Groun(450,595,900,10);
 
    tank1 = new Tank(60,580,100,20);
    tank2 = new Tank(60,200,100,20);
